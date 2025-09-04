99e0236e4c88e86acb75b549ae2604ee3efc0d25
ceeb0t <nxrek@gmail.com>
2025-09-04 15:09:22 +0100

    Merge branch 'argoapp'

--- Terraform/argocd/argocd.tf
+++ Terraform/argocd/argocd.tf
@@ -6,3 +6,13 @@ resource "helm_release" "argocd" {
   version          = "8.3.3"
   values = [file("./argocd/values.yaml")]
 }
+
+resource "helm_release" "argocd-apps" {
+  name = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "argocd"
+  version          = "2.0.2"
+  depends_on       = [helm_release.argocd]
+  values = [file("./argocd/values_apps.yaml")]
+}
