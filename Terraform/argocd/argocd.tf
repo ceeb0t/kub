2771b67bab78d86dc73bf3274304de4333e771a6
ceeb0t <nxrek@gmail.com>
2025-09-04 14:31:21 +0100

    argo testing

--- Terraform/argocd/argocd.tf
+++ Terraform/argocd/argocd.tf
@@ -6,3 +6,12 @@ resource "helm_release" "argocd" {
   version          = "8.3.3"
   values = [file("./argocd/values.yaml")]
 }
+
+resource "helm_release" "argocd-apps" {
+  name = "argocd-apps"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argocd-apps"
+  namespace        = "argocd"
+  version          = "1.4.1"
+  values = [file("./argocd/values_apps.yaml")]
+}
